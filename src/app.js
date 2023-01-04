98e533064a18a57226860edce6ade11791af40b1
Louise Peridy <louiseperidy@MacBook-Pro-de-Louise.local>
2023-01-04 14:00:03 +0100

    test gsap

--- src/app.js
+++ src/app.js
@@ -1,14 +1,32 @@
 import { gsap } from "gsap";
+import { MotionPathPlugin} from "gsap/MotionPathPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-console.log('Hello world!');
+var animation;
 
-gsap.to(".box", { 
-    rotation: 360,
-    x: '100vw',
-    xPercent: -100,
-    // special properties
-    duration: 2, // how long the animation lasts
-    repeat: 2, // the number of repeats - this will play 3 times
-    yoyo: true, // this will alternate back and forth on each repeat. Like a yoyo
-  });
-  
+gsap.registerPlugin(MotionPathPlugin, ScrollTrigger);
+
+gsap.set("#motionSVG", { scale: 0.7, autoAlpha: 1 });
+gsap.set("#tractor", {transformOrigin: "50% 50%"});
+
+animation = gsap.to("#motionSVG", {
+  scrollTrigger: {
+    trigger: "#motionPath",
+    start: "top 20%",
+    // end: '+=5000px',
+    scrub: 1,
+    markers: true,
+    onUpdate: self => {
+      gsap.to("#tractor", {rotation: () => self.direction === 1 ? 0 : -180, overwrite: 'auto'});
+    }
+  },
+  duration: 10,
+  ease: "none",
+  immediateRender: true,
+  motionPath: {
+    path: "#motionPath",
+    align: "#motionPath",
+    alignOrigin: [0.5, 0.5],
+    autoRotate: 90,
+  }
+});
