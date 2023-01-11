9c0cddec5605291a292600302ba958a4f0ed3d1b
LouisePrd <louise.peridy22@gmail.com>
2023-01-11 15:24:22 +0100

    fix plane

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { AxesHelper } from "three";
 import { DragControls } from "three/examples/jsm/controls/DragControls.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
@@ -20,6 +21,9 @@ function onWindowResize() {
 
 const scene = new THREE.Scene();
 
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
 const materials = [
   new THREE.MeshBasicMaterial({ color: 0xff0000 }),
   new THREE.MeshBasicMaterial({ color: 0x0000ff }),
@@ -31,31 +35,24 @@ const materials = [
 
 // Add Image to the scene : doe
 const loader = new THREE.TextureLoader();
-const texture = loader.load(
-  "assets/les_biches_marie_laurencin.png"
-);
+const texture = loader.load("assets/les_biches_marie_laurencin.png");
 const geometry = new THREE.PlaneGeometry(0.5, 0.5);
 const material = new THREE.MeshBasicMaterial({ map: texture });
 const mesh = new THREE.Mesh(geometry, material);
 mesh.position.set(0, 0, -1);
 // scene.add(mesh);
 
-// Add rectangle to the scene
-const geometryPlane = new THREE.PlaneGeometry(0.6, 0.6);
-const materialPlane = new THREE.MeshBasicMaterial( {color: 689582, side: THREE.DoubleSide, opacity: 0.01} );
-const plane = new THREE.Mesh( geometryPlane, materialPlane );
+// Add rectangle to the scene to set the painting
+const geometryPlane = new THREE.PlaneGeometry(0.62, 0.43);
+const materialPlane = new THREE.MeshBasicMaterial({
+  color: 689582,
+  side: THREE.DoubleSide,
+  opacity: 0.01,
+});
+const plane = new THREE.Mesh(geometryPlane, materialPlane);
 plane.material.transparent = true;
 plane.position.set(0, 0, -1);
-
-scene.add( plane );
-
-// Cube in the ground
-const cubeGround = new THREE.Mesh(
-  new THREE.BoxGeometry(0.2, 0.2, 0.2),
-  materials
-);
-cubeGround.position.set(0, -0.5, -1);
-// scene.add(cubeGround);
+scene.add(plane);
 
 // Tab for Drag Controls
 const dragObjetcs = [plane];
@@ -80,21 +77,23 @@ controls.addEventListener("dragend", function (event) {
   event.object.material.emissive.set(0x000000);
 });
 
-
-// test pinch
-window.addEventListener('gestureend', function(e) {
-  let x = plane.scale.x;
-  let y = plane.scale.y;
-  let z = plane.scale.z;
-  if (e.scale < 1.0) {
-      plane.scale.set(x-0.1, y-0.1, z-0.1);
-  } else if (e.scale > 1.0) {
-    plane.scale.set(x+0.1, y+0.1, z+0.1);
-  }
-}, false);
+// Pinch to resize
+window.addEventListener(
+  "gestureend",
+  function (e) {
+    let x = plane.scale.x;
+    let y = plane.scale.y;
+    let z = plane.scale.z;
+    if (e.scale < 1.0) {
+      plane.scale.set(x - 0.1, y - 0.1, z - 0.1);
+    } else if (e.scale > 1.0) {
+      plane.scale.set(x + 0.1, y + 0.1, z + 0.1);
+    }
+  },
+  false
+);
 
 async function activateXR() {
-  // Initialize a WebXR session using "immersive-ar".
   const session = await navigator.xr.requestSession("immersive-ar");
   session.updateRenderState({
     baseLayer: new XRWebGLLayer(session, gl),
@@ -104,7 +103,6 @@ async function activateXR() {
 
   // Create a render loop that allows us to draw on the AR view.
   const onXRFrame = (time, frame) => {
-    // Queue up the next draw request.
     session.requestAnimationFrame(onXRFrame);
 
     // Bind the graphics framebuffer to the baseLayer's framebuffer
@@ -128,14 +126,14 @@ async function activateXR() {
       camera.updateMatrixWorld(true);
 
       // Render loop
-      var render = function(){
+      var render = function () {
         renderer.render(scene, camera);
-        requestAnimationFrame(render); 
-      }
+        requestAnimationFrame(render);
+      };
 
       render();
 
-      // ancienne loop (au cas ou hihi)
+      // ancienne loop (au cas où hihi)
       // renderer.setAnimationLoop(function () {
       //   orbitControls.update();
       //   renderer.render(scene, camera);
