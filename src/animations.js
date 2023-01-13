154340f9143dc81673995c5718d024421738c657
Lwiz <77757761+LouisePrd@users.noreply.github.com>
2023-01-13 01:22:15 +0100

    first page back ok

--- src/animations.js
+++ src/animations.js
@@ -1,56 +1,78 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  // ===== Variables =====
+  // Intro pages
+  var intro1 = document.getElementById("intro1");
+  var intro2 = document.getElementById("intro2");
+  var intro3 = document.getElementById("intro3");
 
-window.addEventListener('DOMContentLoaded', (event) => {
-    // ===== Variables =====
-    // Intro pages
-    var intro1 = document.getElementById("intro1");
-    var intro2 = document.getElementById("intro2");
-    var intro3 = document.getElementById("intro3");
+  // Buttons
+  var start = document.getElementById("buttonStart");
+  var back = document.getElementById("back");
 
-    // Buttons
-    var start = document.getElementById("buttonStart");
-    var back = document.getElementById("back");
+  // Articles
+  let article1 = document.getElementById("cubisme");
+  let article2 = document.getElementById("nymphisme");
+  let article3 = document.getElementById("feminisme");
+  let article4 = document.getElementById("artiste");
+  let article5 = document.getElementById("lesBiches");
 
-    // Articles
-    let allArticles = document.getElementsByClassName("article");
+  back.addEventListener("click", (event) => {
+    article1.style.top = "120vh";
+    article2.style.top = "120vh";
+    article3.style.top = "120vh";
+    article4.style.top = "120vh";
+    article5.style.top = "120vh";
 
-    // ===== Introduction animation =====
+    setTimeout(() => {
+      article1.style.display = "none";
+      article2.style.display = "none";
+      article3.style.display = "none";
+      article4.style.display = "none";
+      article5.style.display = "none";
+    }, 2000);
+  });
 
-    // appear(intro1);
-    // setTimeout(() => {
-    //     disappear(intro1);
-    //   }, 4000)
-    // setTimeout(() => {
-    //     appear(intro2);
-    // }, 5600)
-    // setTimeout(() => {
-    //     disappear(intro2);
-    // }, 8000)
-    // setTimeout(() => {
-    //     appear(intro3);
-    // }, 9000)
-    // setTimeout(() => {
-    //     start.style.letterSpacing = "4px";
-    // }, 9000)
+  // Articles
+  let allArticles = document.getElementsByClassName("article");
 
-    // ===== Articles animation =====
+  // ===== Introduction animation =====
+
+  // appear(intro1);
+  // setTimeout(() => {
+  //     disappear(intro1);
+  //   }, 4000)
+  // setTimeout(() => {
+  //     appear(intro2);
+  // }, 5600)
+  // setTimeout(() => {
+  //     disappear(intro2);
+  // }, 8000)
+  // setTimeout(() => {
+  //     appear(intro3);
+  // }, 9000)
+  // setTimeout(() => {
+  //     start.style.letterSpacing = "4px";
+  // }, 9000)
+
+  // ===== Articles animation =====
 });
 
 // ===== FUNCTIONS =====
 
 function appear(target) {
-    target.style.opacity = 1;
+  target.style.opacity = 1;
 }
 
 function disappear(target) {
-    target.style.opacity = 0;
-    setTimeout(() => {
+  target.style.opacity = 0;
+  setTimeout(() => {
     target.style.display = "none";
-    }, 1700)
+  }, 1700);
 }
 
 function triggerArticle(article) {
-    article.style.display = "block";
-    setTimeout(() => {
+  article.style.display = "block";
+  setTimeout(() => {
     article.style.top = "0";
-    }, 100);
+  }, 100);
 }
